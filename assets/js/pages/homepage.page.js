c331356e46f08b67ecabedc78b68418fc41dbda1
Jan1907 <janfluk83@gmail.com>
2023-11-20 17:06:36 +0100

    mehr und weniger anzeigen funktion

---
+++ assets/js/pages/homepage.page.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var textToShow = document.getElementById('textToShow');
+    var additionalText = document.getElementById('additionalText');
+    var toggleLink = document.getElementById('toggleLink');
+  
+    toggleLink.addEventListener('click', function () {
+      // Überprüfen Sie den Status und ändern Sie den Text entsprechend
+      if (additionalText.style.display === 'none' || additionalText.style.display === '') {
+        additionalText.style.display = 'inline'; // oder 'block', je nach Bedarf
+        toggleLink.textContent = 'Weniger Anzeigen';
+      } else {
+        additionalText.style.display = 'none';
+        toggleLink.textContent = 'Mehr Anzeigen ';
+      }
+    });
+  });
